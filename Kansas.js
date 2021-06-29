8e00c65a6f8d4f2b8c165703a2a57f8d2e380fb3
angtucker5 <78523256+angtucker5@users.noreply.github.com>
2021-06-29 15:53:39 -0500

    Add files via upload

---
+++ Kansas.js
@@ -0,0 +1,242 @@
+var data = [{
+		"Candidate": "Jerry Moran",
+		"Contributor": "Live Nation Entertainment",
+		"Total": 20500
+	},{
+		"Candidate": "Jerry Moran",
+		"Contributor": "Spirit Aerosystems",
+		"Total": 20050
+	},{
+		"Candidate": "Jerry Moran",
+		"Contributor": "Hilltop Holdings",
+		"Total": 18400
+	},{
+		"Candidate": "Jerry Moran",
+		"Contributor": "Veterans United Home Loans",
+		"Total": 15000
+	},{
+		"Candidate": "Jerry Moran",
+		"Contributor": "Triwest Healthcare Alliance",
+		"Total": 14100
+	},{
+		"Candidate": "Jerry Moran",
+		"Contributor": "Hallmark Cards",
+		"Total": 12504
+	},{
+		"Candidate": "Jerry Moran",
+		"Contributor": "Van Scoyoc Assoc",
+		"Total": 11800
+	},{
+		"Candidate": "Jerry Moran",
+		"Contributor": "Anschutz Corp",
+		"Total": 11200
+	},{
+		"Candidate": "Jerry Moran",
+		"Contributor": "ClearPath Foundation",
+		"Total": 11200
+	},{
+		"Candidate": "Jerry Moran",
+		"Contributor": "Northrock Inc",
+		"Total": 11200
+	},{
+		"Candidate": "Pat Roberts",
+		"Contributor": "National Grocers Assn",
+		"Total": 4500
+	},{
+		"Candidate": "Pat Roberts",
+		"Contributor": "Bristol-Myers Squibb",
+		"Total": 1000
+	},{
+		"Candidate": "Pat Roberts",
+		"Contributor": "CVS Health",
+		"Total": 1000
+	},{
+		"Candidate": "Pat Roberts",
+		"Contributor": "Barclays",
+		"Total": 500
+	},{
+		"Candidate": "Pat Roberts",
+		"Contributor": "AmerisourceBergen Corp",
+		"Total": 0
+	},{
+		"Candidate": "Pat Roberts",
+		"Contributor": "Amgen Inc",
+		"Total": 0
+	},{
+		"Candidate": "Pat Roberts",
+		"Contributor": "Blue Cross/Blue Shield",
+		"Total": 0
+	},{
+		"Candidate": "Pat Roberts",
+		"Contributor": "Burns & McDonnell",
+		"Total": 0
+	},{
+		"Candidate": "Pat Roberts",
+		"Contributor": "California Dairies Inc",
+		"Total": 0
+	},{
+		"Candidate": "Pat Roberts",
+		"Contributor": "CME Group",
+		"Total": 0
+	},{
+		"Candidate": "Roger Marshall",
+		"Contributor": "Nueterra Capital",
+		"Total": 43600
+	},{
+		"Candidate": "Roger Marshall",
+		"Contributor": "Poet LLC",
+		"Total": 32500
+	},{
+		"Candidate": "Roger Marshall",
+		"Contributor": "Burns & McDonnell",
+		"Total": 22036
+	},{
+		"Candidate": "Roger Marshall",
+		"Contributor": "Susan B Anthony List",
+		"Total": 20200
+	},{
+		"Candidate": "Roger Marshall",
+		"Contributor": "Watco Companies",
+		"Total": 20000
+	},{
+		"Candidate": "Roger Marshall",
+		"Contributor": "Goldman Sachs",
+		"Total": 19100
+	},{
+		"Candidate": "Roger Marshall",
+		"Contributor": "Bukaty Companies",
+		"Total": 18900
+	},{
+		"Candidate": "Roger Marshall",
+		"Contributor": "Bristol-Myers Squibb",
+		"Total": 17600
+	},{
+		"Candidate": "Roger Marshall",
+		"Contributor": "Innovative Livestock Services",
+		"Total": 16800
+	},{
+		"Candidate": "Roger Marshall",
+		"Contributor": "Koch Industries",
+		"Total": 16635
+	},{
+		"Candidate": "Ron Estes",
+		"Contributor": "Spirit Aerosystems",
+		"Total": 21550
+	},{
+		"Candidate": "Ron Estes",
+		"Contributor": "Poet LLC",
+		"Total": 21200
+	},{
+		"Candidate": "Ron Estes",
+		"Contributor": "Icm Inc",
+		"Total": 14050
+	},{
+		"Candidate": "Ron Estes",
+		"Contributor": "Wichita Railway Service",
+		"Total": 13700
+	},{
+		"Candidate": "Ron Estes",
+		"Contributor": "Cox Enterprises",
+		"Total": 13000
+	},{
+		"Candidate": "Ron Estes",
+		"Contributor": "Morgan Stanley",
+		"Total": 12000
+	},{
+		"Candidate": "Ron Estes",
+		"Contributor": "Blackstone Group",
+		"Total": 11450
+	},{
+		"Candidate": "Ron Estes",
+		"Contributor": "Aviation Consulting & Engineering Solu",
+		"Total": 11200
+	},{
+		"Candidate": "Ron Estes",
+		"Contributor": "Cornejo Construction",
+		"Total": 11200
+	},{
+		"Candidate": "Ron Estes",
+		"Contributor": "Lrico Services",
+		"Total": 11200
+	},{
+		"Candidate": "Sharice Davids",
+		"Contributor": "Emily's List",
+		"Total": 52150
+	},{
+		"Candidate": "Sharice Davids",
+		"Contributor": "Democracy Engine",
+		"Total": 35650
+	},{
+		"Candidate": "Sharice Davids",
+		"Contributor": "United for A Strong America",
+		"Total": 26800
+	},{
+		"Candidate": "Sharice Davids",
+		"Contributor": "Dentons",
+		"Total": 23100
+	},{
+		"Candidate": "Sharice Davids",
+		"Contributor": "League of Conservation Voters",
+		"Total": 20839
+	},{
+		"Candidate": "Sharice Davids",
+		"Contributor": "PAC to the Future",
+		"Total": 20000
+	},{
+		"Candidate": "Sharice Davids",
+		"Contributor": "Deutsche Telekom",
+		"Total": 18486
+	},{
+		"Candidate": "Sharice Davids",
+		"Contributor": "University of Kansas",
+		"Total": 17956
+	},{
+		"Candidate": "Sharice Davids",
+		"Contributor": "Service Employees International Union",
+		"Total": 17605
+	},{
+		"Candidate": "Sharice Davids",
+		"Contributor": "US Government",
+		"Total": 16541
+	},{
+		"Candidate": "Steve Watkins",
+		"Contributor": "Armstrong Farms",
+		"Total": 11800
+	},{
+		"Candidate": "Steve Watkins",
+		"Contributor": "Bk2 Holdings",
+		"Total": 11200
+	},{
+		"Candidate": "Steve Watkins",
+		"Contributor": "Macerich Co",
+		"Total": 11200
+	},{
+		"Candidate": "Steve Watkins",
+		"Contributor": "O'Brate Foundation",
+		"Total": 11200
+	},{
+		"Candidate": "Steve Watkins",
+		"Contributor": "Marcus Foundation",
+		"Total": 11198
+	},{
+		"Candidate": "Steve Watkins",
+		"Contributor": "Western National Group",
+		"Total": 11096
+	},{
+		"Candidate": "Steve Watkins",
+		"Contributor": "Station Casinos",
+		"Total": 10976
+	},{
+		"Candidate": "Steve Watkins",
+		"Contributor": "Watco Companies",
+		"Total": 10800
+	},{
+		"Candidate": "Steve Watkins",
+		"Contributor": "Crow Holdings",
+		"Total": 8406
+	},{
+		"Candidate": "Steve Watkins",
+		"Contributor": "Community Oncology Alliance",
+		"Total": 7500
+	}
+];
